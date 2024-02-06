b21eb7af9141237684596ab8f40a5b021ea37fd9
Bolo Chen <bolosstuff12@gmail.com>
2024-02-06 16:04:16 +0700

    Add Update Product Feature

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 import { logger } from '../utils/logger'
-import { createProductValidation } from '../validations/product.validation'
-import { addProductToDB, getProductById, getProductFromDB } from '../services/product.service'
+import { createProductValidation, updateProductValidation } from '../validations/product.validation'
+import { addProductToDB, getProductById, getProductFromDB, updateProductById } from '../services/product.service'
 import { v4 as uuid4 } from 'uuid'
 
 export const createProduct = async (req: Request, res: Response) => {
@@ -14,7 +14,7 @@ export const createProduct = async (req: Request, res: Response) => {
   try {
     await addProductToDB(value)
     logger.info('Success add new product data')
-    return res.status(201).send({ status: true, statusCode: 200, message: 'Add product success' })
+    return res.status(201).send({ status: true, statusCode: 201, message: 'Add product success' })
   } catch (error) {
     logger.error('ERR: product - create = ', error)
     return res.status(422).send({ status: false, statuscode: 422, message: error })
@@ -39,4 +39,22 @@ export const getProduct = async (req: Request, res: Response) => {
     logger.info('Success get prodcut data')
     return res.status(200).send({ status: true, statusCode: '200', data: products })
   }
+}
+
+export const updateProduct = async (req: Request, res: Response) => {
+  const {
+    params: { id }
+  } = req
+
+  const { error, value } = updateProductValidation(req.body)
+  if (error) {
+    logger.error('ERR: product - create = ', error.details[0].message)
+    return res.status(422).send({ status: false, statuscode: 422, message: error.details[0].message, data: {} })
+  }
+
+  try {
+    await updateProductById(id, value)
+    logger.info('Success update product data')
+    return res.status(200).send({ status: true, statusCode: 200, message: 'Update product success' })
+  } catch (error) {}
 }
